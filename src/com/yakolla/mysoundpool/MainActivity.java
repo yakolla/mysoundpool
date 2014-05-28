3df4ece6e629ae1ae26259f20587ae084aaf3cb8
yakolla <yakolla@gmail.com>
2014-05-28 17:52:21 +0900

    동시에 소리 나는지 테스트

--- src/com/yakolla/mysoundpool/MainActivity.java
+++ src/com/yakolla/mysoundpool/MainActivity.java
@@ -16,7 +16,7 @@ public class MainActivity extends Activity {
 	
 	private SoundPool sPool; // 사운드 풀
 	private AudioManager mAudioManager;
-	
+	private int	loadedCount = 0;
 	public static final String TAG = "MYSOUNDPOOL";
 	
 	/** Called when the activity is first created. */
@@ -31,13 +31,23 @@ public class MainActivity extends Activity {
 		sPool = new SoundPool(3, AudioManager.STREAM_MUSIC, 0);
 		sPool.setOnLoadCompleteListener(new OnLoadCompleteListener() {
 		    public void onLoadComplete(SoundPool soundPool, int sampleId,int status) {
-		    	float volume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
-		    	soundPool.play(sampleId, volume, volume, 0, 0, 1);
+		    	++loadedCount;
+		    	Log.d(TAG, "sampleid: " + sampleId);
+		    	
+		    	if (loadedCount == 2)
+		    	{
+		    		float volume = mAudioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+			    	soundPool.play(1, volume, volume, 0, 0, 1);
+			    	soundPool.play(2, volume, volume, 0, 0, 1);			    		
+		    	}
+		    	
 		    }
 		});
 
 		try {
-			sPool.load(getResources().getAssets().openFd("katy_perry-dark_horse.mid"), 1);			
+			
+			sPool.load(getResources().getAssets().openFd("Bach__Prelude_in_C_major.mid"), 1);
+			sPool.load(getResources().getAssets().openFd("katy_perry-dark_horse.mid"), 1);	
 			
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
